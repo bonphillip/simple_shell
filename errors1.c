0f2f106ab36b106facead9c1af62613568f143eb
bonphillip <bonsoluzions@gmail.com>
2023-10-13 04:14:10 -0700

    simple shell

---
+++ errors1.c
@@ -0,0 +1,99 @@
+#include "shell.h"
+
+/**/
+int stringToInteger(char *str) {
+    int index = 0;
+    unsigned long int result = 0;
+
+    if (*str == '+')
+        str++;
+    for (index = 0; str[index] != '\0'; index++) {
+        if (str[index] >= '0' && str[index] <= '9') {
+            result *= 10;
+            result += (str[index] - '0');
+            if (result > INT_MAX)
+                return -1;
+        } else {
+            return -1;
+        }
+    }
+    return (int)result;
+}
+
+void printErrorMessage(info_t *info, char *errorString) {
+    print(info->filename);
+    print(": ");
+    printDecimal(info->lineCount, STDERR_FILENO);
+    print(": ");
+    print(info->arguments[0]);
+    print(": ");
+    print(errorString);
+}
+
+int printDecimal(int number, int fd) {
+    int (*putCharacter)(char) = putchar;
+    int i, count = 0;
+    unsigned int absoluteValue, current;
+
+    if (fd == STDERR_FILENO)
+        putCharacter = eputchar;
+
+    if (number < 0) {
+        absoluteValue = -number;
+        putCharacter('-');
+        count++;
+    } else {
+        absoluteValue = number;
+    }
+    
+    current = absoluteValue;
+    for (i = 1000000000; i > 1; i /= 10) {
+        if (absoluteValue / i) {
+            putCharacter('0' + current / i);
+            count++;
+        }
+        current %= i;
+    }
+    
+    putCharacter('0' + current);
+    count++;
+    return count;
+}
+
+char *convertToBase(long int num, int base, int flags) {
+    static char *array;
+    static char buffer[50];
+    char sign = 0;
+    char *ptr;
+    unsigned long n = num;
+
+    if (!(flags & CONVERT_UNSIGNED) && num < 0) {
+        n = -num;
+        sign = '-';
+    }
+    
+    array = (flags & CONVERT_LOWERCASE) ? "0123456789abcdef" : "0123456789ABCDEF";
+    ptr = &buffer[49];
+    *ptr = '\0';
+
+    do {
+        *--ptr = array[n % base];
+        n /= base;
+    } while (n != 0);
+
+    if (sign)
+        *--ptr = sign;
+    
+    return ptr;
+}
+
+void removeComments(char *buffer) {
+    int index;
+
+    for (index = 0; buffer[index] != '\0'; index++) {
+        if (buffer[index] == '#' && (!index || buffer[index - 1] == ' ')) {
+            buffer[index] = '\0';
+            break;
+        }
+    }
+}
---
+++ exits.c
@@ -0,0 +1,75 @@
+#include "custom_lib.h"
+
+/**
+ * my_strncpy - Custom string copy function
+ * @destination: The destination string to copy to
+ * @source: The source string
+ * @num_chars: The max no of characters to copy
+ * Return: A pointer to the copied string
+ */
+char *my_strncpy(char *destination, char *source, int num_chars)
+{
+	int i, j;
+	char *result = destination;
+
+	i = 0;
+	while (source[i] != '\0' && i < num_chars - 1)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	if (i < num_chars)
+	{
+		j = i;
+		while (j < num_chars)
+		{
+			destination[j] = '\0';
+			j++;
+		}
+	}
+	return result;
+}
+
+/**
+ * my_strncat - Custom string concatenation function
+ * @dest: The first string
+ * @src: The second string to be concatenated
+ * @max_bytes: The maximum no of bytes to use
+ * Return: A pointer to the concatenated string
+ */
+char *my_strncat(char *dest, char *src, int max_bytes)
+{
+	int i, j;
+	char *result = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < max_bytes)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < max_bytes)
+		dest[i] = '\0';
+	return result;
+}
+
+/**
+ * my_strchr - Custom character search in a string
+ * @str: The string to search
+ * @character: The character to search
+ * Return: A pointer to the memory area having the character or NULL if not found
+ */
+char *my_strchr(char *str, char character)
+{
+	do {
+		if (*str == character)
+			return str;
+	} while (*str++ != '\0');
+
+	return NULL;
+}
+
